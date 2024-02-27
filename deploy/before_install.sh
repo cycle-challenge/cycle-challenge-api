aa87be308552b4d045dd2c51a1efea1583060b6f
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-02-27 19:13:35 +0900

    build(cd): CD 관련 스크립트 수정, 추가
    - 이전 배포를 삭제하는 before_install 스크립트 추가 (API 버전 UP 대응)
    - stop application에서 포트 PID를 찾아 중지하도록 변경

---
+++ deploy/before_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -rf /home/ubuntu/build/*
+echo "> 이전 배포 파일 정리 " >> /home/ubuntu/build/deploy.log
--- deploy/stop_application.sh
+++ deploy/stop_application.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-echo "> 이전 서버의 프로세스 중지" >> /home/ubuntu/build/deploy.log
-sudo pkill -f 'java -jar'
+
+# 8080 포트를 사용하는 프로세스의 PID 찾기
+PID=$(lsof -ti:8080)
+
+# PID가 존재하면 (즉, 해당 포트를 사용하는 프로세스가 있으면)
+if [ ! -z "$PID" ]; then
+  echo "> 8080 포트를 사용하는 이전 서버 프로세스 PID($PID) 중지" >> /home/ubuntu/build/deploy.log
+  # 프로세스 종료
+  sudo kill $PID
+else
+  echo "> 8080 포트를 사용하는 서버 프로세스 없음" >> /home/ubuntu/build/deploy.log
+fi
