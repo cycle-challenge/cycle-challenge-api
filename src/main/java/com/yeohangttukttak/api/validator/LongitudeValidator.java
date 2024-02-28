8a7e56d7576e1b5c8a373e0a7c5966d85ad734e3
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-02-28 12:58:36 +0900

    feat(validation): 위도, 경도 커스텀 Validtor 구현
    - @Range 및 @DecimalMax가 실수를 제대로 처리하지 못하는 이슈 발견
    - 커스텀 위도, 경도 검증 Validator를 구현항ㅆ음

---
+++ src/main/java/com/yeohangttukttak/api/validator/LongitudeValidator.java
@@ -0,0 +1,12 @@
+package com.yeohangttukttak.api.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+class LongitudeValidator implements ConstraintValidator<ValidLongitude, Double> {
+
+    @Override
+    public boolean isValid(Double longitude, ConstraintValidatorContext context) {
+        return longitude != null && longitude >= -180 && longitude <= 180;
+    }
+}
