9aded39e7b8baab8cab01ca7900c3c36d9b03206
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-02-28 12:57:14 +0900

    feat(validation): 오류 응답을 위한 클래스 생성
    - 에러 코드, 상태를 ENUM으로 관리하는 ApiErrorCode 생성
    - ApiError는 Enum을 받아 stats, name을 설정함
    - 에러 응답은 이 ApiError를 여러 개 받도록 구성

---
+++ src/main/java/com/yeohangttukttak/api/controller/ApiError.java
@@ -0,0 +1,22 @@
+package com.yeohangttukttak.api.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ApiError {
+
+    private int status;
+
+    private String code;
+
+    private String message;
+
+    public ApiError(ApiErrorCode code, String message) {
+        this.status = code.getStatus();
+        this.code = code.name();
+        this.message = message;
+    }
+
+}
